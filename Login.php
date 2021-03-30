6a106669cdbe60c539ded3fc1ab72f616e941a66
LordBamm <80847326+LordBamm@users.noreply.github.com>
2021-03-30 15:04:02 -0500

    LoginUpdate
    Changed the login page to submit to itself.

--- Login.php
+++ Login.php
@@ -1,26 +1,58 @@
 <?php
     require "header.php";
-    $invalidLogin = filter_input(INPUT_GET, 'invalid', FILTER_VALIDATE_INT);
+
+    $invalidLogin = false;
+    $username = "";
+    $password = "";
+
+    if($_POST)
+    {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        $query = "SELECT * FROM users WHERE UserName = :username AND Password = :password";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':password', $password);
+        $statement->execute();
+
+        $user = $statement->fetch();
+
+        if($statement->rowCount() !== 0)
+        {
+            $_SESSION["username"] = $user["UserName"];
+            $_SESSION["userId"] = $user["UserId"];
+            $_SESSION["role"] = $user["Role"];
+    
+            header("Location: index.php");
+            exit();
+        }
+        else
+        {
+            $invalidLogin = true;
+        }
+    } 
 ?>
 
 
-<form action=verification.php method="post">
+<form action=# method="post">
     <fieldset>
-    <h2>Login</h2>
+        <h2>Login</h2>
         <p>
             <label for="Username">Username:</label>
-            <input name="username" id="username" />
-        </p>
+            <input name="username" id="username" value="<?=$username?>"/>
+        </p>  
         <p>
             <label for="password">Password:</label>
-            <input name="password" id="password"/>
+            <input name="password" id="password" value="<?=$password ?>"/>
         </p>
+        <?php if($invalidLogin) : ?>
+            <p>Invalid username or password</p>
+        <?php endif; ?>
         <p>
             <input type="submit" name="login" value="Login" />
         </p>
-        <?php if(isset($invalidLogin)) : ?>
-            <p>Invalid username or password</p>
-        <?php endif; ?>
+
         <p><a href="register.php">Register</a></p>
     </fieldset>
 </form>
